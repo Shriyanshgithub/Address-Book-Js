427b129246c64a2bc7a97ef20207cef71f649939
Shriyansh Gupta <gshriyansh703@gmail.com>
2025-03-04 07:40:39 +0530

    [Shriyansh Gupta] Add. uc7 remove duplicate address book

--- addressBook.js
+++ addressBook.js
@@ -77,6 +77,9 @@ function countAddressBook(){
     return addressBookArray.reduce((length,_) => length+1,0);
 }
 
-
-
+//uc-7
+function removeDuplicates(){
+    let uniqueAddressBooks = [...new Set(addressBookArray)];
+    addressBookArray = uniqueAddressBooks;
+}
 
